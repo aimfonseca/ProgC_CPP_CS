f95916d8efdadd869d099124fa5eae43ee4021df
cintiahills2 <cintiahills@gmail.com>
2024-10-24 19:03:26 +0100

    24/10/24
    tarefas

---
+++ Cíntia/aula_24-10-24/OOP01.24-10_CV.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+//definição da classe
+class nomeClasse{
+    //declaração de membros privados desta classe
+    private:
+        int valorPrivado;
+ 
+    //declaração de membros públicos desta classe
+    public:
+        int valorPublico;
+ 
+        void funcaoPublica(){
+            cout << "Esta é a minha primeira função" << endl;
+        }
+}; // Fim da classe 'nomeClasse', termina com ';'
+ 
+int main(){
+    //instanciando um objeto da classe 'nomeClasse'
+    nomeClasse objetoNovo;
+ 
+    //Podemos aceder e modificar os dados de um objeto público
+    objetoNovo.valorPublico = 10;
+ 
+    //Também podemos usar as funções públicas definidas dentro da classe
+    objetoNovo.funcaoPublica();
+}
