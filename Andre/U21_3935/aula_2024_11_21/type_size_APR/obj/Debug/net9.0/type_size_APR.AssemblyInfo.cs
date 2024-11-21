7d30d7bd505420dc5d8cd381977719fdca5a3251
André Proença <161771666+Cordyceps13@users.noreply.github.com>
2024-11-21 15:11:31 +0000

    test

--- Andre/U21_3935/aula_2024_11_21/type_size_APR/obj/Debug/net9.0/type_size_APR.AssemblyInfo.cs
+++ Andre/U21_3935/aula_2024_11_21/type_size_APR/obj/Debug/net9.0/type_size_APR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("type_size_APR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ddeb4c23c310b4cc97c2d6b6468c579dc4f163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf5567f2fbd1374fedc9a524782349cae401bcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("type_size_APR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("type_size_APR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
