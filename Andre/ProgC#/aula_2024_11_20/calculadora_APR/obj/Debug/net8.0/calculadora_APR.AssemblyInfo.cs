42a0687b4d5016f388827c90890e427bce81f689
André Proença <161771666+Cordyceps13@users.noreply.github.com>
2024-11-21 14:40:57 +0000

    up

--- Andre/ProgC#/aula_2024_11_20/calculadora_APR/obj/Debug/net8.0/calculadora_APR.AssemblyInfo.cs
+++ Andre/ProgC#/aula_2024_11_20/calculadora_APR/obj/Debug/net8.0/calculadora_APR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora_APR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7edb970fb75612372631637994099224d26451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603d0e8819d50f85afed73f00f52b59f85ba2aac")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora_APR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora_APR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Andre/ProgC#/aula_2024_11_21/type_size_APR/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        
+        Carimbo.ExibirInformacoes();
+
+        Console.WriteLine("Tamanho em bytes de um SByte: " + sizeof(SByte));
+        Console.WriteLine("Valor mínimo de um SByte: " + SByte.MinValue);
+        Console.WriteLine("Valor máximo de um SByte: " + SByte.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Byte: " + sizeof(Byte));
+        Console.WriteLine("Valor mínimo de um Byte: " + Byte.MinValue);
+        Console.WriteLine("Valor máximo de um Byte: " + Byte.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Boolean: " + sizeof(Boolean));
+        Console.WriteLine("Valor mínimo de um Boolean: " + Boolean.FalseString);
+        Console.WriteLine("Valor máximo de um Boolean: " + Boolean.TrueString);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Char: " + sizeof(Char));
+        Console.WriteLine("Valor mínimo de um Char: " + Char.MinValue);
+        Console.WriteLine("Valor máximo de um Char: " + Char.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Short: " + sizeof(Int16));
+        Console.WriteLine("Valor mínimo de um Short: " + Int16.MinValue);
+        Console.WriteLine("Valor máximo de um Short: " + Int16.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um UShort: " + sizeof(UInt16));
+        Console.WriteLine("Valor mínimo de um UShort: " + UInt16.MinValue);
+        Console.WriteLine("Valor máximo de um UShort: " + UInt16.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Integer: " + sizeof(Int32));
+        Console.WriteLine("Valor mínimo de um Integer: " + Int32.MinValue);
+        Console.WriteLine("Valor máximo de um Integer: " + Int32.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Long: " + sizeof(Int64));
+        Console.WriteLine("Valor mínimo de um Long: " + Int64.MinValue);
+        Console.WriteLine("Valor máximo de um Long: " + Int64.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um ULong: " + sizeof(UInt64));
+        Console.WriteLine("Valor mínimo de um ULong: " + UInt64.MinValue);
+        Console.WriteLine("Valor máximo de um ULong: " + UInt64.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Float: " + sizeof(Single));
+        Console.WriteLine("Valor mínimo de um Float: " + Single.MinValue);
+        Console.WriteLine("Valor máximo de um Float: " + Single.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Decimal: " + sizeof(Decimal));
+        Console.WriteLine("Valor mínimo de um Decimal: " + Decimal.MinValue);
+        Console.WriteLine("Valor máximo de um Decimal: " + Decimal.MaxValue);
+
+        Console.WriteLine("---------------------------------------------------------------");
+
+        Console.WriteLine("Tamanho em bytes de um Double: " + sizeof(Double));
+        Console.WriteLine("Valor mínimo de um Double: " + Double.MinValue);
+        Console.WriteLine("Valor máximo de um Double: " + Double.MaxValue);
+
+    }
+}
