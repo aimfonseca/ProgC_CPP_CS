ce9c4a9259dbb4676bf971aaad641cf389769c8b
André Proença <161771666+Cordyceps13@users.noreply.github.com>
2024-11-28 14:40:50 +0000

    up

--- Andre/U21_3935/aula_2024_11_28/Prim_proj_WF_APR/Form2.Designer.cs
+++ Andre/U21_3935/aula_2024_11_28/Prim_proj_WF_APR/Form2.Designer.cs
@@ -1,16 +1,9 @@
 ﻿namespace Prim_proj_WF_APR
 {
-    partial class Form1
+    partial class Form2
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,34 +13,16 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.Button0 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            //
-            // Button0
-            //
-            this.Button0.Text =  "Voltar";
-            this.Button0.Location = new System.Drawing.Point(144,144);
-            this.Button0.Size = new System.Drawing.Size(184,48);
-         //
-         // form
-         //
-            this.Size = new System.Drawing.Size(480,400);
-            this.Text =  "Form1";
-            this.Controls.Add(this.Button0);
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(480, 400);
+            this.Name = "Form2";
+            this.Text = "Segunda Janela - Form2";
             this.ResumeLayout(false);
-        } 
-
-        #endregion 
-
-        private System.Windows.Forms.Button Button0;
+        }
     }
 }
-
