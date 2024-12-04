d040c95eb687185783a3a80f87876436174fdd43
André Proença <161771666+Cordyceps13@users.noreply.github.com>
2024-12-04 09:21:32 +0000

    up

--- Andre/U21_3935/aula_2024_12_04/Calculadora/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
+++ Andre/U21_3935/aula_2024_12_04/Calculadora/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("primeiro_prog_APR")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923c33ca0533038f64256fc8971adecdcd59c9e7")]
-[assembly: System.Reflection.AssemblyProductAttribute("primeiro_prog_APR")]
-[assembly: System.Reflection.AssemblyTitleAttribute("primeiro_prog_APR")]
+[assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
