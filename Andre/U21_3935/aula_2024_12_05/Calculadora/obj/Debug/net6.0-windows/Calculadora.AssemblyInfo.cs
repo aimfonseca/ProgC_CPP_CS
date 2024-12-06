e2838e9b920baec16e0e99be5141416c54873e72
André Proença <161771666+Cordyceps13@users.noreply.github.com>
2024-12-06 16:47:10 +0000

    update

--- Andre/U21_3935/aula_2024_12_05/Calculadora/obj/Debug/net6.0-windows/Calculadora.AssemblyInfo.cs
+++ Andre/U21_3935/aula_2024_12_05/Calculadora/obj/Debug/net6.0-windows/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83672de0d5a17e445c732917d99f5653242c77b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48cd4c34af102c55d7a60389c966916e9bead643")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
