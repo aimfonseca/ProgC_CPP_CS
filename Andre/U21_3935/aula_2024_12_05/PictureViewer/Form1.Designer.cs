48cd4c34af102c55d7a60389c966916e9bead643
André Proença <161771666+Cordyceps13@users.noreply.github.com>
2024-12-06 14:12:17 +0000

    update repo

---
+++ Andre/U21_3935/aula_2024_12_05/PictureViewer/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace PictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            checkBox1 = new CheckBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            botao_img = new Button();
+            botao_limpar = new Button();
+            botao_escolher = new Button();
+            botao_fechar = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            colorDialog1 = new ColorDialog();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(checkBox1, 0, 1);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 1, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(808, 568);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            tableLayoutPanel1.SetColumnSpan(pictureBox1, 2);
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(802, 505);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Dock = DockStyle.Fill;
+            checkBox1.Location = new Point(3, 514);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(115, 51);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Esticar Imagem";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(botao_img);
+            flowLayoutPanel1.Controls.Add(botao_limpar);
+            flowLayoutPanel1.Controls.Add(botao_escolher);
+            flowLayoutPanel1.Controls.Add(botao_fechar);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.RightToLeft;
+            flowLayoutPanel1.Location = new Point(124, 514);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(681, 51);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // botao_img
+            // 
+            botao_img.AutoSize = true;
+            botao_img.Location = new Point(570, 3);
+            botao_img.Name = "botao_img";
+            botao_img.Size = new Size(108, 25);
+            botao_img.TabIndex = 0;
+            botao_img.Text = "Escolher Imagem";
+            botao_img.UseVisualStyleBackColor = true;
+            botao_img.Click += botao_img_Click;
+            // 
+            // botao_limpar
+            // 
+            botao_limpar.AutoSize = true;
+            botao_limpar.Location = new Point(489, 3);
+            botao_limpar.Name = "botao_limpar";
+            botao_limpar.Size = new Size(75, 25);
+            botao_limpar.TabIndex = 1;
+            botao_limpar.Text = "Limpar";
+            botao_limpar.UseVisualStyleBackColor = true;
+            botao_limpar.Click += botao_limpar_Click;
+            // 
+            // botao_escolher
+            // 
+            botao_escolher.AutoSize = true;
+            botao_escolher.Location = new Point(351, 3);
+            botao_escolher.Name = "botao_escolher";
+            botao_escolher.Size = new Size(132, 25);
+            botao_escolher.TabIndex = 2;
+            botao_escolher.Text = "Escolher cor de fundo";
+            botao_escolher.UseVisualStyleBackColor = true;
+            botao_escolher.Click += botao_escolher_Click;
+            // 
+            // botao_fechar
+            // 
+            botao_fechar.AutoSize = true;
+            botao_fechar.Location = new Point(270, 3);
+            botao_fechar.Name = "botao_fechar";
+            botao_fechar.Size = new Size(75, 25);
+            botao_fechar.TabIndex = 3;
+            botao_fechar.Text = "Fechar";
+            botao_fechar.UseVisualStyleBackColor = true;
+            botao_fechar.Click += botao_fechar_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            openFileDialog1.Title = "Escolher imagem";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(808, 568);
+            Controls.Add(tableLayoutPanel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Visualizador de Imagens Cordyceps ®";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox1;
+        private CheckBox checkBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button botao_img;
+        private Button botao_limpar;
+        private Button botao_escolher;
+        private Button botao_fechar;
+        private OpenFileDialog openFileDialog1;
+        private ColorDialog colorDialog1;
+    }
+}
---
+++ Andre/U21_3935/aula_2024_12_05/Quizz/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Quizz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            time_label = new Label();
+            label1 = new Label();
+            plusLeftLabel = new Label();
+            mais = new Label();
+            plusRightLabel = new Label();
+            label4 = new Label();
+            soma = new NumericUpDown();
+            diferenca = new NumericUpDown();
+            label3 = new Label();
+            minusRightLabel = new Label();
+            menos = new Label();
+            minusLeftLabel = new Label();
+            produto = new NumericUpDown();
+            label8 = new Label();
+            timesRightLabel = new Label();
+            vezes = new Label();
+            timesLeftLabel = new Label();
+            quociente = new NumericUpDown();
+            label12 = new Label();
+            dividedRightLabel = new Label();
+            divisao = new Label();
+            dividedLeftLabel = new Label();
+            StartBtn = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)soma).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)diferenca).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)produto).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)quociente).BeginInit();
+            SuspendLayout();
+            // 
+            // time_label
+            // 
+            time_label.BorderStyle = BorderStyle.FixedSingle;
+            time_label.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            time_label.Location = new Point(310, 9);
+            time_label.Name = "time_label";
+            time_label.Size = new Size(162, 35);
+            time_label.TabIndex = 0;
+            time_label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(148, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Tempo restante";
+            // 
+            // plusLeftLabel
+            // 
+            plusLeftLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            plusLeftLabel.Location = new Point(58, 75);
+            plusLeftLabel.Name = "plusLeftLabel";
+            plusLeftLabel.Size = new Size(60, 50);
+            plusLeftLabel.TabIndex = 2;
+            plusLeftLabel.Text = "?";
+            plusLeftLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // mais
+            // 
+            mais.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            mais.Location = new Point(124, 75);
+            mais.Name = "mais";
+            mais.Size = new Size(60, 50);
+            mais.TabIndex = 3;
+            mais.Text = "+";
+            mais.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // plusRightLabel
+            // 
+            plusRightLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            plusRightLabel.Location = new Point(190, 75);
+            plusRightLabel.Name = "plusRightLabel";
+            plusRightLabel.Size = new Size(60, 50);
+            plusRightLabel.TabIndex = 4;
+            plusRightLabel.Text = "?";
+            plusRightLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(256, 75);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 50);
+            label4.TabIndex = 5;
+            label4.Text = "=";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // soma
+            // 
+            soma.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            soma.Location = new Point(322, 82);
+            soma.MaximumSize = new Size(100, 0);
+            soma.Name = "soma";
+            soma.Size = new Size(100, 39);
+            soma.TabIndex = 1;
+            // 
+            // diferenca
+            // 
+            diferenca.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            diferenca.Location = new Point(322, 144);
+            diferenca.MaximumSize = new Size(100, 0);
+            diferenca.Name = "diferenca";
+            diferenca.Size = new Size(100, 39);
+            diferenca.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(256, 137);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 50);
+            label3.TabIndex = 10;
+            label3.Text = "=";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // minusRightLabel
+            // 
+            minusRightLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            minusRightLabel.Location = new Point(190, 137);
+            minusRightLabel.Name = "minusRightLabel";
+            minusRightLabel.Size = new Size(60, 50);
+            minusRightLabel.TabIndex = 9;
+            minusRightLabel.Text = "?";
+            minusRightLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // menos
+            // 
+            menos.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            menos.Location = new Point(124, 137);
+            menos.Name = "menos";
+            menos.Size = new Size(60, 50);
+            menos.TabIndex = 8;
+            menos.Text = "-";
+            menos.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // minusLeftLabel
+            // 
+            minusLeftLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            minusLeftLabel.Location = new Point(58, 137);
+            minusLeftLabel.Name = "minusLeftLabel";
+            minusLeftLabel.Size = new Size(60, 50);
+            minusLeftLabel.TabIndex = 7;
+            minusLeftLabel.Text = "?";
+            minusLeftLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // produto
+            // 
+            produto.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            produto.Location = new Point(322, 204);
+            produto.MaximumSize = new Size(100, 0);
+            produto.Name = "produto";
+            produto.Size = new Size(100, 39);
+            produto.TabIndex = 3;
+            // 
+            // label8
+            // 
+            label8.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(256, 197);
+            label8.Name = "label8";
+            label8.Size = new Size(60, 50);
+            label8.TabIndex = 15;
+            label8.Text = "=";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // timesRightLabel
+            // 
+            timesRightLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            timesRightLabel.Location = new Point(190, 197);
+            timesRightLabel.Name = "timesRightLabel";
+            timesRightLabel.Size = new Size(60, 50);
+            timesRightLabel.TabIndex = 14;
+            timesRightLabel.Text = "?";
+            timesRightLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // vezes
+            // 
+            vezes.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            vezes.Location = new Point(124, 197);
+            vezes.Name = "vezes";
+            vezes.Size = new Size(60, 50);
+            vezes.TabIndex = 13;
+            vezes.Text = "x";
+            vezes.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // timesLeftLabel
+            // 
+            timesLeftLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            timesLeftLabel.Location = new Point(58, 197);
+            timesLeftLabel.Name = "timesLeftLabel";
+            timesLeftLabel.Size = new Size(60, 50);
+            timesLeftLabel.TabIndex = 12;
+            timesLeftLabel.Text = "?";
+            timesLeftLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // quociente
+            // 
+            quociente.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            quociente.Location = new Point(322, 263);
+            quociente.MaximumSize = new Size(100, 0);
+            quociente.Name = "quociente";
+            quociente.Size = new Size(100, 39);
+            quociente.TabIndex = 4;
+            // 
+            // label12
+            // 
+            label12.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.Location = new Point(256, 256);
+            label12.Name = "label12";
+            label12.Size = new Size(60, 50);
+            label12.TabIndex = 20;
+            label12.Text = "=";
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dividedRightLabel
+            // 
+            dividedRightLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            dividedRightLabel.Location = new Point(190, 256);
+            dividedRightLabel.Name = "dividedRightLabel";
+            dividedRightLabel.Size = new Size(60, 50);
+            dividedRightLabel.TabIndex = 19;
+            dividedRightLabel.Text = "?";
+            dividedRightLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // divisao
+            // 
+            divisao.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            divisao.Location = new Point(124, 256);
+            divisao.Name = "divisao";
+            divisao.Size = new Size(60, 50);
+            divisao.TabIndex = 18;
+            divisao.Text = "÷";
+            divisao.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dividedLeftLabel
+            // 
+            dividedLeftLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            dividedLeftLabel.Location = new Point(58, 256);
+            dividedLeftLabel.Name = "dividedLeftLabel";
+            dividedLeftLabel.Size = new Size(60, 50);
+            dividedLeftLabel.TabIndex = 17;
+            dividedLeftLabel.Text = "?";
+            dividedLeftLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // StartBtn
+            // 
+            StartBtn.AutoSize = true;
+            StartBtn.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            StartBtn.Location = new Point(157, 342);
+            StartBtn.Name = "StartBtn";
+            StartBtn.Size = new Size(150, 35);
+            StartBtn.TabIndex = 0;
+            StartBtn.Text = "Começar Quizz";
+            StartBtn.UseVisualStyleBackColor = true;
+            StartBtn.Click += startBtn_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(480, 413);
+            Controls.Add(StartBtn);
+            Controls.Add(quociente);
+            Controls.Add(label12);
+            Controls.Add(dividedRightLabel);
+            Controls.Add(divisao);
+            Controls.Add(dividedLeftLabel);
+            Controls.Add(produto);
+            Controls.Add(label8);
+            Controls.Add(timesRightLabel);
+            Controls.Add(vezes);
+            Controls.Add(timesLeftLabel);
+            Controls.Add(diferenca);
+            Controls.Add(label3);
+            Controls.Add(minusRightLabel);
+            Controls.Add(menos);
+            Controls.Add(minusLeftLabel);
+            Controls.Add(soma);
+            Controls.Add(label4);
+            Controls.Add(plusRightLabel);
+            Controls.Add(mais);
+            Controls.Add(plusLeftLabel);
+            Controls.Add(label1);
+            Controls.Add(time_label);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Quizz Cordyceps®";
+            ((System.ComponentModel.ISupportInitialize)soma).EndInit();
+            ((System.ComponentModel.ISupportInitialize)diferenca).EndInit();
+            ((System.ComponentModel.ISupportInitialize)produto).EndInit();
+            ((System.ComponentModel.ISupportInitialize)quociente).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label time_label;
+        private Label label1;
+        private Label plusLeftLabel;
+        private Label mais;
+        private Label plusRightLabel;
+        private Label label4;
+        private NumericUpDown soma;
+        private NumericUpDown diferenca;
+        private Label label3;
+        private Label minusRightLabel;
+        private Label menos;
+        private Label minusLeftLabel;
+        private NumericUpDown produto;
+        private Label label8;
+        private Label timesRightLabel;
+        private Label vezes;
+        private Label timesLeftLabel;
+        private NumericUpDown quociente;
+        private Label label12;
+        private Label dividedRightLabel;
+        private Label divisao;
+        private Label dividedLeftLabel;
+        private Button StartBtn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
