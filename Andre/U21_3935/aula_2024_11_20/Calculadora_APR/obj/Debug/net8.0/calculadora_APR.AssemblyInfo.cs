2615e117433ed6ce9a1a69c9f5e1db277d891a70
MrPinkfloydian <pinkfloydian05@gmail.com>
2024-12-06 15:42:48 +0000

    Actualização

--- Andre/U21_3935/aula_2024_11_20/Calculadora_APR/obj/Debug/net8.0/calculadora_APR.AssemblyInfo.cs
+++ Andre/U21_3935/aula_2024_11_20/Calculadora_APR/obj/Debug/net8.0/calculadora_APR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora_APR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603d0e8819d50f85afed73f00f52b59f85ba2aac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc29b5ecffe33b228e04863a2ccc2648b0ba5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora_APR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora_APR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
